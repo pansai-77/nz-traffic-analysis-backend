2ce819bca42da896ed5785adfb0d9cfd56e3489b
pansai-77 <pansai@pansaideMacBook-Air.local>
2026-01-20 18:48:38 +0800

    modify name

--- src/main/java/io/github/pansai/traffic/TrafficAnalysisApplication.java
+++ src/main/java/io/github/pansai/traffic/TrafficAnalysisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NZTrafficAnalysisBackendApplication {
+public class TrafficAnalysisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NZTrafficAnalysisBackendApplication.class, args);
+        SpringApplication.run(TrafficAnalysisApplication.class, args);
     }
 
 }
